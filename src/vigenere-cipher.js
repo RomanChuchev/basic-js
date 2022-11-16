94fb88009f0a221403a3cecf2595ac1bcd97b34c
Roman Chuchev <romanok20@gmail.com>
2022-11-16 11:55:29 +0300

    finish task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,73 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 class VigenereCipheringMachine {
-    encrypt() {
-        throw new NotImplementedError("Not implemented");
-        // remove line with error and write your code here
+    constructor(isDirectMachine = true) {
+        this.isDirectMachine = isDirectMachine;
     }
-    decrypt() {
-        throw new NotImplementedError("Not implemented");
-        // remove line with error and write your code here
+
+    static get ALPHABET_LENGTH() {
+        return 26;
+    }
+
+    static get ALPHABET_START_CODE() {
+        return 65;
+    }
+
+    encrypt(message, key) {
+        if (typeof message !== "string" || typeof key !== "string") {
+            throw new Error("Incorrect arguments!");
+        }
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        const cipherText = [];
+        let pos = 0;
+        for (const ch of message) {
+            if (ch < "A" || ch > "Z") {
+                cipherText.push(ch);
+            } else {
+                const msgCode = ch.charCodeAt(0);
+                const keyCode = key.charCodeAt(pos % key.length);
+                const cipherChar = String.fromCharCode(
+                    ((msgCode + keyCode) %
+                        VigenereCipheringMachine.ALPHABET_LENGTH) +
+                        VigenereCipheringMachine.ALPHABET_START_CODE
+                );
+                cipherText.push(cipherChar);
+                pos++;
+            }
+        }
+        return this.isDirectMachine
+            ? cipherText.join("")
+            : cipherText.reverse().join("");
+    }
+
+    decrypt(encryptedMessage, key) {
+        if (typeof encryptedMessage !== "string" || typeof key !== "string") {
+            throw new Error("Incorrect arguments!");
+        }
+        key = key.toUpperCase();
+        const plainText = [];
+        let pos = 0;
+        for (const ch of encryptedMessage) {
+            if (ch < "A" || ch > "Z") {
+                plainText.push(ch);
+            } else {
+                const msgCode = ch.charCodeAt(0);
+                const keyCode = key.charCodeAt(pos % key.length);
+                const plainChar = String.fromCharCode(
+                    ((msgCode -
+                        keyCode +
+                        VigenereCipheringMachine.ALPHABET_LENGTH) %
+                        VigenereCipheringMachine.ALPHABET_LENGTH) +
+                        VigenereCipheringMachine.ALPHABET_START_CODE
+                );
+                plainText.push(plainChar);
+                pos++;
+            }
+        }
+        return this.isDirectMachine
+            ? plainText.join("")
+            : plainText.reverse().join("");
     }
 }
 
